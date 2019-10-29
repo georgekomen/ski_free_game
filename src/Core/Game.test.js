fbe31b64bcbdc259f089de604c49ea2946ea1820
komen <gkkomensi@gmail.com>
2019-10-29 21:09:31 +0300

    add one more unit test

--- src/Core/Game.test.js
+++ src/Core/Game.test.js
@@ -1,24 +1,30 @@
 import "babel-polyfill";
-import { Skier } from "../Entities/Skier";
-import { Rhino } from "../Entities/Rhino";
 import { Game } from "./Game";
 
 describe('game class', () => {
     const game = new Game();
     jest.useFakeTimers();
 
-    test('game should be able to restart', () => {
+    test('rhino should wake up some time after game starts', () => {
         const scheduleToWakeUpRhino = jest.spyOn(game, 'scheduleToWakeUpRhino');
-        game.skier.eaten();
-        game.restartGame();
-        jest.advanceTimersByTime(2000);
+
+        game.scheduleToWakeUpRhino();
+        jest.advanceTimersByTime(20000);
 
         expect(scheduleToWakeUpRhino).toHaveBeenCalled();
+        expect(game.rhino.isAwake).toBeTruthy();
+    });
+
+    test('game should be able to restart and rhino should hide', () => {
+        game.skier.eaten();
+        game.restartGame();
+
+        expect(game.rhino.isAwake).toBeFalsy();
     });
 
     test('game ended when skier is not alive', () => {
         game.skier.eaten();
         expect(game.gameEnded()).toBe(true);
-    })
+    });
 
 });
