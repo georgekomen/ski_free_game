a873fdb8e48becf2177c3ea14412061100fd86ef
komen <gkkomensi@gmail.com>
2019-10-30 09:31:49 +0300

    added kubernetes deployment file that deploys our app to a cluster in gcp

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
+npm run test
 npm run build
 docker build -t gcr.io/trusty-solution-256615/ski-free-game .
-gcloud auth configure-docker
-#docker login -u _json_key -p "$(cat /home/komen/Downloads/permissions.json)" https://gcr.io #only run once (the path is path to the credentials file -got from google IMA and Admin)
+# gcloud auth configure-docker - run once
 docker push gcr.io/trusty-solution-256615/ski-free-game
