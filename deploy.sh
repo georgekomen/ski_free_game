cbb15b45de1c1dd838b7b4b73d2251fba4ca47d7
komen <gkkomensi@gmail.com>
2019-12-09 13:08:42 +0300

    add continuos intergration and deployment with jenkins

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 npm run test
 npm run build
-docker build -t gcr.io/trusty-solution-256615/ski-free-game:dis .
+docker build -t gcr.io/trusty-solution-256615/ski-free-game:dis2 .
 # gcloud auth configure-docker - run once
 docker push gcr.io/trusty-solution-256615/ski-free-game
 kubectl apply -f kubernetes/deployment.yml
